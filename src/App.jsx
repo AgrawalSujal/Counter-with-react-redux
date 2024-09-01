3cb997335120e4b57c13d134dd73436f0c7e2224
AgrawalSujal <agrawalsujal6522@gmail.com>
2024-09-01 12:52:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-// import store from "./store/store.jsx";
 import { add, sub } from "./slice/update";
 import { useDispatch, useSelector } from "react-redux";
 import store from "./store/store.jsx";
-// import {useDispatch} from "react-redux";
+
 
 function App() {
   const dispatch = useDispatch();
